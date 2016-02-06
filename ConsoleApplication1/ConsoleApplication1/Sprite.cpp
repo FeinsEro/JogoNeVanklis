ba021b87cf3f6155080f33a83bb92f74e76793ef
Arthur Mendes <arthurmco@gmail.com>
2016-02-06 19:16:47 -0200

    Finished design of Sprite.cpp

--- ConsoleApplication1/ConsoleApplication1/Sprite.cpp
+++ ConsoleApplication1/ConsoleApplication1/Sprite.cpp
@@ -8,6 +8,8 @@ Sprite::Sprite(const char* path, int frame_width, int frame_height){
 	this->sheet_path = std::string(path);
 	this->_frame_width = frame_width;
 	this->_frame_height = frame_height;
+	this->_frame_cache = NULL;
+
 	UpdateSheet();
 
 }
@@ -24,12 +26,43 @@ void Sprite::UpdateSheet() {
 
 	this->_frame_count = (_image_width / _frame_width) * (_image_height / _frame_height);
 
+	this->_frame_num = 0;
+	this->_frame_x = 0;
+	this->_frame_y = 0;
+
 }
 ALLEGRO_BITMAP* Sprite::NextFrame(){
-	
+	return this->StepFrames(1);	
 }
-ALLEGRO_BITMAP* Sprite::PreviousFrame(){}
-ALLEGRO_BITMAP* Sprite::StepFrames(int){}
+ALLEGRO_BITMAP* Sprite::PreviousFrame(){
+	return this->StepFrames(-1);
+}
+ALLEGRO_BITMAP* Sprite::StepFrames(int num) {
+	
+	this->_frame_num += num;
 
-int Sprite::GetFrameNumber(){}
-ALLEGRO_BITMAP* Sprite::GetFrameImage(){}
+	if (this->_frame_num < 0) {
+		this->_frame_num = 0; //loop agora não
+	}
+
+	if (this->_frame_num > this->_frame_count) {
+		this->_frame_num = this->_frame_count;
+	}
+
+	this->_frame_y = (num * _frame_width) / _image_width;
+	this->_frame_x = (num * _frame_width) % _image_width;
+	
+	//Talvez usar unique_ptr no bitmap do frame
+	this->_frame_cache = al_create_sub_bitmap(this->_sheet_cache, this->_frame_x, this->_frame_y,
+		this->_frame_width, this->_frame_height);
+
+	return this->_frame_cache;
+}
+
+int Sprite::GetFrameNumber() {
+	return this->_frame_num;
+}
+
+ALLEGRO_BITMAP* Sprite::GetFrameImage() {
+	return this->_frame_cache;
+}
