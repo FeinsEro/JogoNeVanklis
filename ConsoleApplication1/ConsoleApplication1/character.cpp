55f9afee8c62a4e16c3c5a0545ceb7c6446a4681
Arthur Mendes <arthurmco@gmail.com>
2016-02-06 17:37:30 -0200

    fixed compilation erros in character.cpp

--- ConsoleApplication1/ConsoleApplication1/character.cpp
+++ ConsoleApplication1/ConsoleApplication1/character.cpp
@@ -2,7 +2,9 @@
 #include <string>
 
 
-Character::Character(unsigned int XPos = 0, unsigned int YPos = 0, std::string name = "", unsigned int HP = 0, unsigned int dinheiro = 0, unsigned int ATK = 0, unsigned int DEF = 0, unsigned int labia = 0)
+Character::Character(unsigned int XPos, unsigned int YPos, std::string name,
+	unsigned int HP, unsigned int dinheiro, unsigned int ATK, 
+	unsigned int DEF, unsigned int labia)
 {
 	this->_XPos = XPos;
 	this->_YPos = YPos;
