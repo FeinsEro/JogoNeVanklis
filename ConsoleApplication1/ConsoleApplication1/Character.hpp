65417b0c0f25862c26aee0aed15114b04d243352
FeinsEro <bruno_queiroz_96@hotmail.com>
2016-02-07 13:26:58 -0200

    Atualizado Medo e Sanidade no Character

--- ConsoleApplication1/ConsoleApplication1/Character.hpp
+++ ConsoleApplication1/ConsoleApplication1/Character.hpp
@@ -8,7 +8,7 @@ protected:
 
 
 	std::string _name;
-	unsigned int _HP, _ATK, _DEF, _labia, _dinheiro;
+	unsigned int _HP, _ATK, _DEF, _labia, _dinheiro, _medo, _sanidade;
 public:
 	inline std::string GetName() { return this->_name; }
 	inline void SetName(std::string name) { this->_name = name; }
@@ -21,6 +21,6 @@ public:
 	virtual void Lutar() = 0;
 	virtual void Reviver() = 0;
 
-	Character(unsigned int XPos = 0, unsigned int YPos = 0, std::string name = "", unsigned int HP = 0, unsigned int dinheiro = 0, unsigned int ATK = 0, unsigned int DEF = 0, unsigned int labia = 0);
+	Character(unsigned int XPos = 0, unsigned int YPos = 0, std::string name = "", unsigned int HP = 0, unsigned int dinheiro = 0, unsigned int ATK = 0, unsigned int DEF = 0, unsigned int labia = 0, unsigned int medo = 0, unsigned int sanidade = 0);
 
 };
--- ConsoleApplication1/ConsoleApplication1/character.cpp
+++ ConsoleApplication1/ConsoleApplication1/character.cpp
@@ -4,7 +4,7 @@
 
 Character::Character(unsigned int XPos, unsigned int YPos, std::string name,
 	unsigned int HP, unsigned int dinheiro, unsigned int ATK, 
-	unsigned int DEF, unsigned int labia)
+	unsigned int DEF, unsigned int labia, unsigned int medo, unsigned sanidade)
 {
 	this->_XPos = XPos;
 	this->_YPos = YPos;
@@ -13,5 +13,7 @@ Character::Character(unsigned int XPos, unsigned int YPos, std::string name,
 	this->_ATK = ATK;
 	this->_DEF = DEF;
 	this->_labia = labia;
+	this->_medo = medo;
+	this->_sanidade = sanidade;
 
 }
