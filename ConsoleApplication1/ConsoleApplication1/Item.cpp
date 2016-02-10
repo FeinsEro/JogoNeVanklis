210e426df753e5e6ff804585ef22b850b826ad31
FeinsEro <bruno_queiroz_96@hotmail.com>
2016-02-10 10:27:39 -0200

    Adicionados outros atributos no item.cpp e no item.hpp

--- ConsoleApplication1/ConsoleApplication1/Item.cpp
+++ ConsoleApplication1/ConsoleApplication1/Item.cpp
@@ -4,10 +4,27 @@
 
 static unsigned int _lastid = 0;
 
-Item::Item(std::string name, unsigned int value) {
+Item::Item(std::string name, std::string descricao,
+	unsigned int value, int bonusHP, int bonusATK,
+	int bonusDEF, int bonusAMR, int bonusMagicDEF,
+	int bonusLabia, int bonusSanidade, int bonusNoFear,
+	unsigned int bonusXP, unsigned int bonusDinheiro) {
+
+
 	this->_id = _lastid++;
 	this->_name = name;
 	this->_value = value;
+	this->_descricao = descricao;
+	this->_bonusHP = bonusHP;
+	this->_bonusATK = bonusATK;
+	this->_bonusDEF = bonusDEF;
+	this->_bonusAMR = bonusAMR;
+	this->_bonusMagicDEF = bonusMagicDEF;
+	this->_bonusLabia = bonusLabia;
+	this->_bonusSanidade = bonusSanidade;
+	this->_bonusNoFear = bonusNoFear;
+	this->_bonusXP = bonusXP;
+	this->_bonusDinheiro = bonusDinheiro;
 
 
 	fprintf(stderr, "[Item] Item \"%s\" criado, id %d\n", name.c_str(), _id);
--- ConsoleApplication1/ConsoleApplication1/Item.hpp
+++ ConsoleApplication1/ConsoleApplication1/Item.hpp
@@ -9,6 +9,12 @@ private:
 	unsigned int _id;		//id
 	unsigned int _value;	//valor em dinheiro
 	std::string _name;
+	std::string _descricao;
+	int _bonusHP, _bonusATK, _bonusDEF, _bonusAMR, _bonusMagicDEF, _bonusLabia; //valores bonus podem ser negativos também.
+	int _bonusSanidade; //deixei esses dois separados pra dar uma explicação melhor pra eles.
+	int _bonusNoFear; //basicamente, esses valores vão aumentar o quanto o usuário consegue ficar sobre a pressão do mapa e dos monstros sem enlouquecer ou cagar de medo.
+	unsigned int _bonusXP; //caso o item aumente a quantidade de xp que o player consegue.
+	unsigned int _bonusDinheiro; //caso o item aumente a quantidade de dinheiro que o player consegue.
 
 public:
 	inline unsigned int GetID() { return _id; }
@@ -18,6 +24,6 @@ public:
 	inline void GetName(std::string& name) { name = _name; }
 
 
-	Item(std::string name, unsigned int value);
+	Item(std::string name, std::string descricao, unsigned int value, int bonusHP, int bonusATK, int bonusDEF, int bonusAMR, int bonusMagicDEF, int bonusLabia, int bonusSanidade, int bonusNoFear, unsigned int bonusXP, unsigned int bonusDinheiro);
 
 };
