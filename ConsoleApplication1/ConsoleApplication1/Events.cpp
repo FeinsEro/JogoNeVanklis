cb0c8647232267f9f26dcb0d53dd554bf847053a
Arthur Mendes <arthurmco@gmail.com>
2016-08-04 23:51:43 -0300

    Started working on Java refactoring

--- ConsoleApplication1/ConsoleApplication1/Events.cpp
+++
@@ -1,9 +0,0 @@
-#include <allegro5\allegro.h>
-#include "Events.hpp"
-Events::Events() {
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	event_queue = al_create_event_queue();
-	if (!event_queue) {
-		return -1;
-	}
-}
